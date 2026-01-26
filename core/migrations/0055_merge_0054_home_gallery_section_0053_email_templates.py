ff4adb81104210dc59ad02f93b54ef6a8aaa6381
rcc00n <vadrud2016@gmail.com>
2026-01-26 13:20:05 -0700

    Merge email template and home gallery migrations

---
+++ core/migrations/0055_merge_0054_home_gallery_section_0053_email_templates.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0054_home_gallery_section"),
+        ("core", "0053_email_templates"),
+    ]
+
+    operations = []
