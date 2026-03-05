780fbfa327c1fede192ee725e7e3f691feca4973
rcc00n <vadrud2016@gmail.com>
2026-03-05 16:13:28 -0700

    Persist order shipping addresses

--- store/models.py
+++ store/models.py
@@ -789,6 +789,13 @@ class Order(models.Model):
     customer_name = models.CharField(max_length=120)
     email = models.EmailField()
     phone = models.CharField(max_length=32, blank=True)
+    delivery_method = models.CharField(max_length=20, blank=True, default="shipping")
+    address_line1 = models.CharField(max_length=255, blank=True, default="")
+    address_line2 = models.CharField(max_length=255, blank=True, default="")
+    city = models.CharField(max_length=120, blank=True, default="")
+    region = models.CharField(max_length=120, blank=True, default="")
+    postal_code = models.CharField(max_length=32, blank=True, default="")
+    country = models.CharField(max_length=120, blank=True, default="")
 
     # vehicle
     vehicle_make = models.CharField(max_length=64, blank=True)
